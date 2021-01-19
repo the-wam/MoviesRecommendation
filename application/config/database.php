d3e961cbb5f4d5d168f9bdd359eb073336f42fe7
Loic BANCO <45557794+the-wam@users.noreply.github.com>
2021-01-19 12:18:26 +0100

    test prod
    connection database

--- application/config/database.php
+++ application/config/database.php
@@ -4,7 +4,7 @@
  * Database configuration settings used by PDO.
  */
 // restaurant
-$config['dsn']      = 'mysql:host=localhost:3308;dbname=finalmoviesproject;charset=UTF8';
-$config['user']     = 'root';
-$config['password'] = '';
+$config['dsn']      = 'mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_c91a493c94916da;charset=UTF8';
+$config['user']     = 'b893ded4d1eddf';
+$config['password'] = '67bf9b48';
 
